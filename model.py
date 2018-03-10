14d1e9adcf94598d5bca455e145c2a6b18667e70
Lakshmi Sai Ram Thubati <thubatilakshmi@gmail.com>
2018-03-10 12:58:57 -0500

    built grpah and session

--- model.py
+++ model.py
@@ -6,24 +6,74 @@ import matplotlib.pyplot as plt
 X_TRAIN_FNAME = "data/x_train.csv"
 one_hot_length = 60000
 
-def image():
+def image_array():
     with open(X_TRAIN_FNAME,'r') as fh:
         line = fh.readline().strip("\n")
         pixels = line.split(",")
         norm_pixels = [int(pixel)/255.0 for pixel in pixels]
+        norm_pixels = np.array(norm_pixels).astype(np.float32).reshape(1,28,28,1)
         yield norm_pixels
 
+def get_ohvector(index):
+    vector = [0]*one_hot_length
+    vector[index] = 1
+    vector = np.array(vector).reshape(1,one_hot_length)
+    return vector
+
+gph = tf.Graph()
+with gph.as_default():
+    kern_init = tf.random_normal_initializer(mean = 0.0, stddev = 0.01)
+    bias_init = tf.zeros_initializer()
+
+    with tf.variable_scope("inputs"):
+        y_true = tf.placeholder(tf.float32,shape = [1,28,28,1],name = "y_true")
+        x = tf.placeholder(tf.float32, shape = [1,one_hot_length], name = "one_hot_vector")
+    with tf.variable_scope("noise_selector"):
+        noise_vector = tf.get_variable("noise_1d_vector",shape = [one_hot_length,64])
+        noise_selector = tf.matmul(x,noise_vector)
+        noise_mass =tf.reshape(noise_selector,shape = [1,4,4,4],name = "reshaper")
+
+    tf.summary.histogram("noise_vector1d",noise_vector)
+
+    ls_layer_units = [64, 32, 16, 8, 4, 1]
+    ls_layer_names = ["glayer" + str(i + 1) for i in range(6)]
+    layer = noise_mass
+    for i in range(len(ls_layer_units)):
+        with tf.variable_scope(ls_layer_names[i]):
+            layer = tf.layers.conv2d_transpose(layer, ls_layer_units[i], [5, 5],
+                                           strides=[1, 1], padding='valid',
+                                           activation=None,
+                                           use_bias=False,
+                                           kernel_initializer=kern_init,
+                                           trainable=True,name = "conv_transpose")
+            layer = tf.contrib.layers.batch_norm(inputs=layer, center=True, scale=True, is_training=True,
+                                                 scope = "batch_norm")
+            layer = tf.nn.relu(layer,name = "relu")
+
+    gen_image = layer
+    cost = tf.reduce_mean((gen_image - y_true)**2,name = "cost")
+    tf.summary.scalar("cost",cost)
+
+    train = tf.train.AdamOptimizer(1e-2).minimize(cost,name = "optimizer")
+
+    saver = tf.train.Saver([noise_vector])
+    merged = tf.summary.merge_all()
+    writer = tf.summary.FileWriter("checkpoint",graph=tf.get_default_graph())
 
 
-kern_init = tf.random_normal_initializer(mean = 0.0, stddev = 0.01)
-bias_init = tf.zeros_initializer()
+with tf.Session(graph = gph) as sess:
+    sess.run(tf.local_variables_initializer())
+    sess.run(tf.global_variables_initializer())
 
-with tf.variable_scope("inputs"):
-    y_true = tf.placeholder(tf.float32,shape = [1,28,28,1],name = "y_true")
-    x = tf.placeholder(tf.float32, shape = [1,one_hot_length], name = "one_hot_vector")
-with tf.variable_scope("noise_selector"):
-    noise_vector = tf.get_variable("noise_1d_vector",shape = [one_hot_length,64])
-    noise_selector = tf.matmul(x,noise_vector)
-    noise_mass =tf.reshape(noise_selector,shape = [4,4,4],name = "reshaper")
+    for image_index in range(60000):
+        print("\rimage: ".format(image_index)+str(image_index))
+        feed_dict = {x: get_ohvector(image_index),
+                     y_true: next(image_array())}
+        _,summary = sess.run([train,merged],feed_dict)
+        if (image_index+1)%1000 == 0:
+            writer.add_summary(summary,image_index)
 
+    saver.save(sess,"checkpoint/glo")
+    print("Training done")
 
+    #code for evaluating the output goes here
